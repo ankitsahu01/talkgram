ebb11ee488ccf46120cf0ace678e004488860e15
Ankit Sahu <sahu6856@gmail.com>
2022-05-24 23:47:34 +0530

    added comment

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   helmet({
+    // Safely load the data from different origin url, to minimize the cross-site scripting.
     contentSecurityPolicy: {
       useDefaults: true,
       directives: {
